23b4a6f4a07f6c7211d2392df8a6560e9b9db087
Hammad khalid <hammadkhalid607@gmail.com>
2026-05-23 13:20:55 +0500

    all initial files added

--- backend.py
+++ backend.py
@@ -1,1 +1,38 @@
-# Chatbot Backend (LangGraph)
+from langgraph.graph import StateGraph, END, START
+from typing import TypedDict , Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from  langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+
+
+
+class ChatState(TypedDict):
+    messages :Annotated[list[BaseMessage], add_messages]
+
+
+
+def chat_node(state: ChatState):
+    response = llm.invoke(state["messages"])
+    return {"messages": [response]}
+
+
+checkpointer  =  MemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge( "chat_node", END)
+
+chatbot =  graph.compile(checkpointer = checkpointer)
